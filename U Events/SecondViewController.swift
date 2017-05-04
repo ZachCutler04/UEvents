aa4a2baf910e12be220343e4bc2f41c1a9e5f080
Zach Cutler <Zach@Zachs-MacBook-Pro.local>
2017-05-03 19:57:51 -0600

    Initial Commit

---
+++ U Events/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  U Events
+//
+//  Created by Zach Cutler on 5/3/17.
+//  Copyright © 2017 Zach Cutler. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
